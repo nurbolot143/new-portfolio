3c378b10ae08a98d7a37bf5ea36be25229120f4b
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-05-26 15:41:42 +0300

    fixed bug

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,19 @@
 import React, { useRef, useEffect } from "react";
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
-import { init } from "ityped";
+// import { init } from "ityped";
 
 const Intro = () => {
-  const animatedTextRef = useRef();
+  // const animatedTextRef = useRef();
 
-  useEffect(() => {
-    init(animatedTextRef.current, {
-      showCursor: true,
-      backDelay: 5000,
-      backSpeed: 60,
-      placeholder: false,
-      strings: ["Я front-end разработчик."],
-    });
-  }, []);
+  // useEffect(() => {
+  //   init(animatedTextRef.current, {
+  //     showCursor: true,
+  //     backDelay: 5000,
+  //     backSpeed: 60,
+  //     placeholder: false,
+  //     strings: ["Я front-end разработчик."],
+  //   });
+  // }, []);
 
   return (
     <section className="intro">
@@ -22,7 +22,8 @@ const Intro = () => {
           Привет, Меня зовут <span>Нурболот</span>
         </h1>
         <h2>
-          <span ref={animatedTextRef}></span>
+          {/* <span ref={animatedTextRef}></span> */}
+          <span>Я front-end разработчик.</span>
         </h2>
 
         <a href="#aboutme">
